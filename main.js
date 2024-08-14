0d632902fde3d77e48e42c52ad533aaf4052509e
fulcanelly <dallasxfulcanelly@gmail.com>
2024-08-15 01:36:29 +0300

    fut: implement link addition logic

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
-
-// link appedner bot
-// todo
-// * docker compose (1 service)
-// * docker file
-// * take link from env variable
-
-
 const { Telegraf } = require('telegraf')
 const { channelPost } = require('telegraf/filters')
 
 require('dotenv').config()
 
 
-// console.log(process.env.BOT_TOKEN)
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const LINK_TEXT = process.env.LINK_TEXT
+const LINK = process.env.LINK
 
+function appendUrlEntity(text, linkText, url) {
+  const entity = {
+    offset: text.length,
+    length: linkText.length,
+    type: 'text_link',
+    url
+  }
+  return {
+    entity,
+    text: text + linkText
+  }
+}
 
 bot.on(channelPost(), async ctx => {
-
-  const link = '' // тут хз, 
-
+  console.log(ctx.update.channel_post)
   if (ctx.update.channel_post.forward_origin) {
     return
   }
 
   let caption = ctx.update.channel_post.caption
+  const inputText = caption ?? ctx.text
+  const updatedTextNEntity = appendUrlEntity(inputText + '\n\n', LINK_TEXT, LINK)
+  const entites = ctx.update.channel_post.entities || ctx.update.channel_post.caption_entities || []
+  const updatedEntities = entites.concat([updatedTextNEntity.entity])
 
   if (caption) {
-    return await ctx.editMessageCaption(caption + link)
+    return await ctx.editMessageCaption(updatedTextNEntity.text, { caption_entities: updatedEntities })
   }
 
   if (ctx.text) {
-    return await ctx.editMessageText(ctx.text + link)
+    return await ctx.editMessageText(updatedTextNEntity.text, { entities: updatedEntities })
   }
 
 })
